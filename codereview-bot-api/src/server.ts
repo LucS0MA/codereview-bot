11ef34d2b1486b2ce1ed6cc7cac69a91800b31ec
LucS0MA <lucien.kohli@gmail.com>
2026-01-14 16:29:26 +0100

    connection to the github app established + wehbook post test working

--- codereview-bot-api/src/server.ts
+++ codereview-bot-api/src/server.ts
@@ -5,3 +5,10 @@ app.listen(config.port, () => {
     "Hello World!"
     console.log(`Server running on http://localhost:${config.port}`)
 })
+
+app.post('/api/webhooks/github', (req, res) => {
+    console.log('🎯 WEBHOOK REÇU !');
+    console.log('Headers', req.headers);
+    console.log('Body', req.body);
+    res.status(200).send('OK');
+})
